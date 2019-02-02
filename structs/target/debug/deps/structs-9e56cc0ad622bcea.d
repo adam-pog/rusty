b96bfb3f44f645c40f5b34a7e56c35dd671cdde2
Adam Pogwizd <adampog77@gmail.com>
2019-02-01 20:54:25 -0600

    added structs

---
+++ structs/target/debug/deps/structs-9e56cc0ad622bcea.d
@@ -0,0 +1,5 @@
+/home/npzd/devspace/rusty/structs/target/debug/deps/structs-9e56cc0ad622bcea: src/main.rs
+
+/home/npzd/devspace/rusty/structs/target/debug/deps/structs-9e56cc0ad622bcea.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/npzd/devspace/rusty/structs/target/debug/structs: /home/npzd/devspace/rusty/structs/src/main.rs
