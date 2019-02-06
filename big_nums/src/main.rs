f80437a8400af677a7fa34f065cbcdaab808e21b
Adam Pogwizd <adampog77@gmail.com>
2019-02-06 14:31:05 -0600

    rng works with large numbers

--- big_nums/src/main.rs
+++ big_nums/src/main.rs
@@ -1,6 +1,14 @@
 use rug::Integer;
+use rug::rand::RandState;
 
 fn main() {
-    let b = Integer::from(Integer::u_pow_u(29, 40*80*410));
-   // println!("Hello, {:?}", b);
+    let a = Integer::from(Integer::u_pow_u(29, 40*80*410));
+   // let a = a + Integer::from(2);
+    //let b = Integer::from(31);
+   // let c = a.to_string_radix(29);
+    let mut rand = RandState::new();
+//    let bound = a;
+    let i = Integer::from(a.random_below_ref(&mut rand));
+    let t = Integer::from(a.random_below_ref(&mut rand));
+    println!("Hello, {:?}", t);
 }
